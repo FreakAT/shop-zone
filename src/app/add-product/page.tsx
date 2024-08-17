2c4c513bee0d035d587c08d28261b81cafe1587d
Hariom <hariom12062000@gmail.com>
2024-08-17 09:21:50 +0530

    db setup

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,10 @@
+const AddProductPage = () => {
+	return (
+		<div>
+			<h1 className="mb-3 text-lg font-bold">Add Product</h1>
+			<form></form>
+		</div>
+	);
+};
+
+export default AddProductPage;
